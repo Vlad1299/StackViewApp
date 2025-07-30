bf5d69852965e2d22a77e5013f09aaf6afe00407
Vlad <lokotuin12@mail.ru>
2025-07-30 14:45:59 +0300

    Initial Commit

---
+++ StackViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackViewApp
+//
+//  Created by Игнат Гончаров on 30.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
